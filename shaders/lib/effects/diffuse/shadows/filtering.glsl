c07068b238c59b507b1ad5a404c498727b8ed387
Stubman <ameermuqrishmohdzaki@gmail.com>
2021-11-11 10:12:21 +0800

    changed shadow filtering a bit

--- shaders/lib/effects/diffuse/shadows/filtering.glsl
+++ shaders/lib/effects/diffuse/shadows/filtering.glsl
@@ -6,20 +6,20 @@ float getVisibility(in vec3 viewPos, in vec2 coord, in vec3 l)
     vec3 worldPos = viewToWorld(viewPos);
     vec3 shadowPos = worldToShadow(worldPos) * 0.5 + 0.5; // in screen space
 
-    const int pcfSamples = 8;
+    const int pcfSamples = 16;
     const int blockerSamples = 8;
     const float bias = 0.00025;
 
     // Average blocker
-    float radius = 60.0 * (shadowMapResolution * 0.0009765625);
+    float radius = 120.0 * (shadowMapResolution * 0.0009765625);
     float origRadius = radius;
     float blockerResult = 0.0;
     int blockerCount = 0;
     for (int i = 0; i < blockerSamples; i++)
     {
-        float ang = 2.4 * i + interleaved(gl_FragCoord.xy)*M_PI2;
+        float ang = 2.4 * i + rand(coord)*M_PI2;
         vec2 offset = vec2(cos(ang), sin(ang));
-        offset = offset * sqrt((i + 0.5 + rand(coord)) / blockerSamples) * radius;
+        offset = offset * interleaved(gl_FragCoord.xy) * radius;
         offset /= shadowMapResolution;
 
         float sample = texture2D(shadowtex1, shadowPos.xy+offset).r;
@@ -37,9 +37,9 @@ float getVisibility(in vec3 viewPos, in vec2 coord, in vec3 l)
     float result = 0.0;
     for (int i = 0; i < pcfSamples; i++)
     {
-        float ang = 2.4 * i + interleaved(gl_FragCoord.xy)*M_PI2;
+        float ang = 2.4 * i + rand(coord)*M_PI2;
         vec2 offset = vec2(cos(ang), sin(ang));
-        offset = offset * sqrt((i + 0.5 + rand(coord)) / pcfSamples) * radius;
+        offset = offset * interleaved(gl_FragCoord.xy) * radius;
         offset /= shadowMapResolution;
 
         result += step(shadowPos.z - texture2D(shadowtex1, shadowPos.xy+offset).r, bias);
